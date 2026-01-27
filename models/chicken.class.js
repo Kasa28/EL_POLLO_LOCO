4f723fe0a22f5d49801345528d19b7d32c9dba1d
Kasa28 <agliullinaksenija@gmail.com>
2026-01-27 17:44:59 +0100

    change height and width

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,5 +1,8 @@
 class Chicken extends MovableObject{
 
+    y = 330;
+    height = 100;
+
         constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
 
