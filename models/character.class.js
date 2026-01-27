5a12dc0d2288501fbb265f3df14639ccdd9db2e0
Kasa28 <agliullinaksenija@gmail.com>
2026-01-27 21:44:13 +0100

    add control switch so character moves via keyboard only when desired

--- models/character.class.js
+++ models/character.class.js
@@ -2,9 +2,36 @@ class Character extends MovableObject {
 
     height = 300;
     y = 130;
+    images_walking = [
+            'img/2_character_pepe/2_walk/W-21.png',
+            'img/2_character_pepe/2_walk/W-22.png',
+            'img/2_character_pepe/2_walk/W-23.png',
+            'img/2_character_pepe/2_walk/W-24.png',
+            'img/2_character_pepe/2_walk/W-25.png',
+            'img/2_character_pepe/2_walk/W-26.png'
+    ];
+
+    world;
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
+        this.loadImages( this.images_walking);
+
+        this.animation();
+    }
+
+    animation(){
+
+        setInterval(() =>{
+        if(this.world.keyboard.RIGHT) {
+        let i = this.currentImage % this.images_walking.length;
+        let path = this.images_walking[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+        }
+        }, 100);
+
+
     }
 
 
