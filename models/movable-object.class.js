d4d1d42da2045b9712557db5fd2293738ff2d8e2
Kasa28 <agliullinaksenija@gmail.com>
2026-01-27 21:49:24 +0100

    refactor Movableobject add image cache

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,7 +4,9 @@ y = 270;
 img;
 height = 150;
 width = 100;
-
+speed = 0.15;
+imageCache = [];
+    currentImage = 0;
 
 loadImage(path) {
     this.img = new Image(); 
@@ -12,11 +14,22 @@ loadImage(path) {
 
 }
 
+loadImages(arrays) {
+arrays.forEach((path) =>{
+let img = new Image();
+img.src = path;
+this.imageCache[path] = img;
+})
+}
+
 moveRight() {
     console.log('Moving right');
 }
 
-moveLeft() {
+    moveLeft() {
+                setInterval(() => {
+            this.x -= this.speed;
+        }, 1000 / 60);
 
-}
+    }
 }
