99e1d2c410e8b8e58341f9e32f30873055fc7926
Kasa28 <agliullinaksenija@gmail.com>
2026-01-27 21:45:07 +0100

    Randomize chicken movement speed

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -2,12 +2,32 @@ class Chicken extends MovableObject{
 
     y = 330;
     height = 100;
-
+    images_walking = [
+        'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+        'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
+        'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png',
+];  
         constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
 
         this.x = 200 + Math.random() * 500;
+        this.loadImages( this.images_walking);
+
+        this.speed = 0.15 + Math.random() * 0.25;
+
+        this.animation_chicken();
     }
+    animation_chicken(){
+        this.moveLeft();
+
+        setInterval(() =>{
+        let i = this.currentImage % this.images_walking.length;
+        let path = this.images_walking[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+        }, 100)
 
 
-}
+
+    }
+    }
