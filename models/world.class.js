ba4e7751a5bf363d350d3226049c28ecb7e5739b
Kasa28 <agliullinaksenija@gmail.com>
2026-01-28 15:58:43 +0100

    added new function pepe hurt

--- models/world.class.js
+++ models/world.class.js
@@ -8,6 +8,7 @@ canvas;
 ctx;
 keyboard;
 camera_x = 0;
+statusBar = new StatusBar();
 
 
 constructor(canvas, keyboard) {
@@ -16,24 +17,40 @@ constructor(canvas, keyboard) {
     this.keyboard = keyboard;
     this.draw();
     this.setWorld();
+    this.checkCollisions();
 }
 
 setWorld() {
     this.character.world = this;
 }
 
-    draw() {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.ctx.translate(this.camera_x, 0);
-        this.addObjectsToMap(this.level.backgroundObjects);
-        this.addToMap(this.character);
-        this.addObjectsToMap(this.level.enemies);
-        this.addObjectsToMap(this.level.clouds);
-        this.ctx.translate(-this.camera_x, 0);
+checkCollisions() {
+    setInterval(() => {
+        this.level.enemies.forEach( (enemy)=> {
+        if(this.character.isColliding(enemy) ) {
+            this.character.hit();
+            this.statusBar.setPercentage(this.character.energy)
+        }
+    });
+    }, 200);
+}
 
-        let self = this;
-        requestAnimationFrame(function() {
-        self.draw()
+
+draw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.translate(this.camera_x, 0);
+    this.addObjectsToMap(this.level.backgroundObjects);
+    this.ctx.translate(-this.camera_x, 0);
+    this.addToMap(this.statusBar);
+    this.ctx.translate(this.camera_x, 0);
+    this.addToMap(this.character);
+    this.addObjectsToMap(this.level.enemies);
+    this.addObjectsToMap(this.level.clouds);
+    this.ctx.translate(-this.camera_x, 0);
+
+    let self = this;
+    requestAnimationFrame(function() {
+    self.draw()
     });
     }
 
