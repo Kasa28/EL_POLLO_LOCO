4c421b1fed09f8a89c484637c6d95ecfb91a4c41
Kasa28 <agliullinaksenija@gmail.com>
2026-01-28 18:29:10 +0100

    remove blue

--- models/drawable_object.class.js
+++ models/drawable_object.class.js
@@ -22,8 +22,6 @@ draw(ctx) {
 drawFrame(ctx) {
     if(this instanceof Character || this instanceof Chicken) {
     ctx.beginPath();
-    ctx.lineWidth = "5";
-    ctx.strokeStyle = "blue";
     ctx.rect(this.x, this.y, this.width, this.height);
     ctx.stroke();
     }
