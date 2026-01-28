de94f175d97a41e7e843ae4feba7da9c44b2e480
Kasa28 <agliullinaksenija@gmail.com>
2026-01-28 12:05:36 +0100

    refactor

--- models/world.class.js
+++ models/world.class.js
@@ -45,21 +45,25 @@ setWorld() {
 
     addToMap(mo) {
         if(mo.otherDirection) {
-            this.ctx.save();
-            this.ctx.translate(mo.width,0);
-            this.ctx.scale(-1, 1);
-            mo.x = mo.x * -1
+            this.flippImage(mo);
         }
-        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
-        this.ctx.beginPath();
-        this.ctx.lineWidth = "5";
-        this.ctx.strokeStyle = "blue";
-        this.ctx.rect(mo.x, mo.y, mo.x + mo.width, mo.y + mo.height);
-        this.ctx.stroke();
-        if (mo.otherDirection) {
-             mo.x = mo.x * -1
+        mo.draw(this.ctx);
+        mo.drawFrame(this.ctx);
 
-            this.ctx.restore();
+
+        if (mo.otherDirection) {
+            this.flippImageBack(mo);
         }
+    }
+
+    flippImage(mo) {
+        this.ctx.save();
+        this.ctx.translate(mo.width,0);
+        this.ctx.scale(-1, 1);
+        mo.x = mo.x * -1
+    }
+    flippImageBack(mo) {
+             mo.x = mo.x * -1
+            this.ctx.restore();
     }
 }
