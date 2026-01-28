b7cc562c300165ba36d9b2f6b8a7c3e07d88c8bb
Kasa28 <agliullinaksenija@gmail.com>
2026-01-28 15:57:04 +0100

    new folder for collidable

---
+++ models/collidable_object.class.js
@@ -0,0 +1,16 @@
+class CollidableObject extends MovableObject {
+
+    collidable = true;
+    damage = 0;
+    offset = {
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+    };
+
+    canCollide() {
+        
+    }
+
+}
