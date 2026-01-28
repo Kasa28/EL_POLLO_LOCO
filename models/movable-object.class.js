71b2370377643aaaaa1702823c2d363fa074a028
Kasa28 <agliullinaksenija@gmail.com>
2026-01-28 12:11:26 +0100

    addded instaceof only chicken and character

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -36,11 +36,13 @@ draw(ctx) {
 }
 
 drawFrame(ctx) {
+    if(this instanceof Character || this instanceof Chicken) {
     ctx.beginPath();
     ctx.lineWidth = "5";
     ctx.strokeStyle = "blue";
     ctx.rect(this.x, this.y, this.width, this.height);
     ctx.stroke();
+    }
 }
 
 loadImages(arrays) {
