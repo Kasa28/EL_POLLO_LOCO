630c6f312f44df5ec5e8b64ed4fce0202a42fc8e
Kasa28 <agliullinaksenija@gmail.com>
2026-01-29 23:38:59 +0100

    make bottles random

--- models/bottle_under.class.js
+++ models/bottle_under.class.js
@@ -2,9 +2,16 @@ class BottlePickup extends MovableObject {
   width = 60;
   height = 80;
 
+  images_bottles = [
+    "img/6_salsa_bottle/1_salsa_bottle_on_ground.png",
+    "img/6_salsa_bottle/2_salsa_bottle_on_ground.png"
+  ];
+
   constructor() {
-    super().loadImage('img/7_statusbars/3_icons/icon_salsa_bottle.png');
+    super();
+    const randomImg = this.images_bottles[Math.floor(Math.random() * this.images_bottles.length)];
+    this.loadImage(randomImg);
     this.x = 200 + Math.random() * 1800;
-    this.y = 330;
+    this.y = 350;
   }
 }
