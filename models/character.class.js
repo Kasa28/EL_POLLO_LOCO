2322b679559d4de449117e29d35845266d0ea119
Kasa28 <agliullinaksenija@gmail.com>
2026-01-31 13:30:20 +0100

    refactor: extract character image assets into separate file

--- models/character.class.js
+++ models/character.class.js
@@ -1,99 +1,37 @@
 class Character extends MovableObject {
-    height = 300;
-    y = 80;
-    speed = 8;
-    images_walking = [
-            'img/2_character_pepe/2_walk/W-21.png',
-            'img/2_character_pepe/2_walk/W-22.png',
-            'img/2_character_pepe/2_walk/W-23.png',
-            'img/2_character_pepe/2_walk/W-24.png',
-            'img/2_character_pepe/2_walk/W-25.png',
-            'img/2_character_pepe/2_walk/W-26.png'
-    ];
-    images_jumping = [
-        'img/2_character_pepe/3_jump/J-31.png',
-        'img/2_character_pepe/3_jump/J-32.png',
-        'img/2_character_pepe/3_jump/J-33.png',
-        'img/2_character_pepe/3_jump/J-34.png',
-        'img/2_character_pepe/3_jump/J-35.png',
-        'img/2_character_pepe/3_jump/J-36.png',
-        'img/2_character_pepe/3_jump/J-37.png',
-        'img/2_character_pepe/3_jump/J-38.png',
-        'img/2_character_pepe/3_jump/J-39.png',
-    ];
+  height = 300;
+  y = 80;
+  speed = 8;
+  images_walking = CHARACTER_ASSETS.walking;
+  images_jumping = CHARACTER_ASSETS.jumping;
+  images_dead = CHARACTER_ASSETS.dead;
+  images_hurt = CHARACTER_ASSETS.hurt;
+  images_long_idle = CHARACTER_ASSETS.longIdle;
+  images_idle = CHARACTER_ASSETS.idle;
+  idleTime = 0;
+  idleMode = "normal";
+  lastIdleFrame = 0;
+  energy = 100;
+  coins = 0;
+  bottles = 0;
+  world;
+  damage = 20;
+  throwBottles = [];
+  offset = { top: 120, bottom: 30, left: 40, right: 30 };
+  deadFrame = 0;
+  deadPlayedOnce = false;
 
-    images_dead = [
-        'img/2_character_pepe/5_dead/D-51.png',
-        'img/2_character_pepe/5_dead/D-52.png',
-        'img/2_character_pepe/5_dead/D-53.png',
-        'img/2_character_pepe/5_dead/D-54.png',
-        'img/2_character_pepe/5_dead/D-55.png',
-        'img/2_character_pepe/5_dead/D-56.png',
-        'img/2_character_pepe/5_dead/D-57.png',
-    ];
+  constructor() {
+    super().loadImage(CHARACTER_ASSETS.startImage);
+    this.loadImages(this.images_walking);
+    this.loadImages(this.images_jumping);
+    this.loadImages(this.images_dead);
+    this.loadImages(this.images_hurt);
+    this.loadImages(this.images_long_idle);
+    this.loadImages(this.images_idle);
+  }
 
-    images_hurt = [
-        'img/2_character_pepe/4_hurt/H-41.png',
-        'img/2_character_pepe/4_hurt/H-42.png',
-        'img/2_character_pepe/4_hurt/H-43.png',
-    ];
-
-    images_long_idle = [
-        'img/2_character_pepe/1_idle/long_idle/I-11.png',
-        'img/2_character_pepe/1_idle/long_idle/I-12.png',
-        'img/2_character_pepe/1_idle/long_idle/I-13.png',
-        'img/2_character_pepe/1_idle/long_idle/I-14.png',
-        'img/2_character_pepe/1_idle/long_idle/I-15.png',
-        'img/2_character_pepe/1_idle/long_idle/I-16.png',
-        'img/2_character_pepe/1_idle/long_idle/I-17.png',
-        'img/2_character_pepe/1_idle/long_idle/I-18.png',
-        'img/2_character_pepe/1_idle/long_idle/I-19.png',
-        'img/2_character_pepe/1_idle/long_idle/I-20.png'
-    ];
-
-    images_idle = [
-        'img/2_character_pepe/1_idle/idle/I-1.png',
-        'img/2_character_pepe/1_idle/idle/I-2.png',
-        'img/2_character_pepe/1_idle/idle/I-3.png',
-        'img/2_character_pepe/1_idle/idle/I-4.png',
-        'img/2_character_pepe/1_idle/idle/I-5.png',
-        'img/2_character_pepe/1_idle/idle/I-6.png',
-        'img/2_character_pepe/1_idle/idle/I-7.png',
-        'img/2_character_pepe/1_idle/idle/I-8.png',
-        'img/2_character_pepe/1_idle/idle/I-9.png',
-        'img/2_character_pepe/1_idle/idle/I-10.png'
-    ]
-
-    idleTime = 0;
-    idleMode = 'normal';
-    lastIdleFrame = 0;
-    energy = 100;
-    coins = 0;
-    bottles = 0;
-    world;
-    damage = 20;
-    throwBottles = [];
-    offset = {
-        top: 120,
-        bottom: 30,
-        left: 40, 
-        right: 30
-    }
-    deadFrame = 0;  
-    deadPlayedOnce = false;  
-  
-
-    constructor() {
-        super().loadImage('img/2_character_pepe/2_walk/W-21.png');
-        this.loadImages( this.images_walking);
-        this.loadImages( this.images_jumping);
-        this.loadImages( this.images_dead);
-        this.loadImages( this.images_hurt);
-        this.loadImages( this.images_long_idle);
-        this.loadImages( this.images_idle);
-    }
-
-    start() {
+  start() {
     this.applyGravitaty();
     this.animation();
   }
@@ -104,7 +42,7 @@ class Character extends MovableObject {
   }
 
   handleMovement() {
-    if(this.isDead()) return;
+    if (this.isDead()) return;
     if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end) {
       this.moveRight();
       this.otherDirection = false;
@@ -118,16 +56,13 @@ class Character extends MovableObject {
 
   handleAnimations() {
     if (this.isDead()) return this.playDeadOnce();
-    if (this.isDead()) return this.playAnimation(this.images_dead);
     if (this.isHurt()) return this.playAnimation(this.images_hurt);
     if (this.isAboveGround()) return this.playAnimation(this.images_jumping);
     this.handleGroundState();
   }
 
   handleGroundState() {
-    if (this.world.keyboard.UP && !this.isAboveGround()) {
-      this.jump();
-    }
+    if (this.world.keyboard.UP && !this.isAboveGround()) this.jump();
     if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
       this.playAnimation(this.images_walking);
       this.resetIdle();
@@ -139,12 +74,9 @@ class Character extends MovableObject {
   handleIdle() {
     this.idleTime += 50;
     const now = Date.now();
-    if (now - this.lastIdleFrame < 500) return; 
-    if (this.idleTime > 20000) {
-      this.playAnimation(this.images_long_idle);
-    } else {
-      this.playAnimation(this.images_idle);
-    }
+    if (now - this.lastIdleFrame < 500) return;
+    if (this.idleTime > 20000) this.playAnimation(this.images_long_idle);
+    else this.playAnimation(this.images_idle);
     this.lastIdleFrame = now;
   }
 
@@ -157,20 +89,12 @@ class Character extends MovableObject {
     this.speedY = 30;
   }
 
-
   playDeadOnce() {
-  if (!this.deadPlayedOnce) {
+    if (this.deadPlayedOnce) return;
     const lastIndex = this.images_dead.length - 1;
     const i = Math.min(this.deadFrame, lastIndex);
-    const path = this.images_dead[i];
-    this.img = this.imageCache[path];
-    if (this.deadFrame < lastIndex) {
-      this.deadFrame++;
-    } else {
-      this.deadPlayedOnce = true; 
-    }
+    this.img = this.imageCache[this.images_dead[i]];
+    if (this.deadFrame < lastIndex) this.deadFrame++;
+    else this.deadPlayedOnce = true;
   }
 }
-}
-
-  
