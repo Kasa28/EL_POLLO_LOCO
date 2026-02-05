5ab1b468362ff1e91cac44384f40c71ee0185b0c
Kasa28 <agliullinaksenija@gmail.com>
2026-02-05 18:08:43 +0100

    clouds move again

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,18 +1,20 @@
 class Cloud extends MovableObject {
+  y = 20;
+  width = 450;
+  height = 250;
 
-    y = 20;
-    width = 450;
-    height = 250;
+  constructor() {
+    super().loadImage('img/5_background/layers/4_clouds/1.png');
+    this.x = Math.random() * 500;
+    this.speed = 0.2 + Math.random() * 0.3;
+    this.startMoving();
+  }
 
-    constructor() {
-        super().loadImage('img/5_background/layers/4_clouds/1.png');
-        this.x = Math.random() * 500;
+  startMoving() {
+    setInterval(() => this.updateMove(), 1000 / 60);
+  }
 
-
-        this.animation();
-    }
-
-    animation() {
-        this.moveLeft();
-    }
-    }
+  updateMove() {
+    this.moveLeft();
+  }
+}
