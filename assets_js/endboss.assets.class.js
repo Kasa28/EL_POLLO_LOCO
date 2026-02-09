d1ff08af8460b6194fc0d1f16f14184ad2153fb6
Kasa28 <agliullinaksenija@gmail.com>
2026-02-09 17:58:48 +0100

    assets folder

---
+++ assets_js/endboss.assets.class.js
@@ -0,0 +1,30 @@
+const endboss_assets = {
+  walking: [
+    'img/4_enemie_boss_chicken/1_walk/G1.png',
+    'img/4_enemie_boss_chicken/1_walk/G2.png',
+    'img/4_enemie_boss_chicken/1_walk/G3.png',
+    'img/4_enemie_boss_chicken/1_walk/G4.png',
+  ],
+    alert: [
+    'img/4_enemie_boss_chicken/2_alert/G5.png',
+    'img/4_enemie_boss_chicken/2_alert/G6.png',
+    'img/4_enemie_boss_chicken/2_alert/G7.png',
+    'img/4_enemie_boss_chicken/2_alert/G8.png',
+  ],
+    attack: [
+    'img/4_enemie_boss_chicken/3_attack/G13.png',
+    'img/4_enemie_boss_chicken/3_attack/G14.png',
+    'img/4_enemie_boss_chicken/3_attack/G15.png',
+    'img/4_enemie_boss_chicken/3_attack/G16.png',
+  ],
+    hurt: [
+    'img/4_enemie_boss_chicken/4_hurt/G21.png',
+    'img/4_enemie_boss_chicken/4_hurt/G22.png',
+    'img/4_enemie_boss_chicken/4_hurt/G23.png',
+  ],
+    dead: [
+    'img/4_enemie_boss_chicken/5_dead/G24.png',
+    'img/4_enemie_boss_chicken/5_dead/G25.png',
+    'img/4_enemie_boss_chicken/5_dead/G26.png',
+  ],
+};
--- models/endboss.class.js
+++ models/endboss.class.js
@@ -1,109 +1,76 @@
+// models/endboss.class.js
 class Endboss extends MovableObject {
   height = 400;
   width = 250;
   y = 60;
-
   energy = 100;
   isDead = false;
   isActive = false;
   isHurtBoss = false;
   isRemoved = false;
-
   currentState = "walk";
   speed = 1.2;
-
   lastAttack = 0;
   attackCooldown = 1200;
-
-  images_walking = [
-    'img/4_enemie_boss_chicken/1_walk/G1.png',
-    'img/4_enemie_boss_chicken/1_walk/G2.png',
-    'img/4_enemie_boss_chicken/1_walk/G3.png',
-    'img/4_enemie_boss_chicken/1_walk/G4.png',
-  ];
-
-  images_alert = [
-    'img/4_enemie_boss_chicken/2_alert/G5.png',
-    'img/4_enemie_boss_chicken/2_alert/G6.png',
-    'img/4_enemie_boss_chicken/2_alert/G7.png',
-    'img/4_enemie_boss_chicken/2_alert/G8.png',
-    'img/4_enemie_boss_chicken/2_alert/G9.png',
-    'img/4_enemie_boss_chicken/2_alert/G10.png',
-    'img/4_enemie_boss_chicken/2_alert/G11.png',
-    'img/4_enemie_boss_chicken/2_alert/G12.png'
-  ];
-
-  images_attack = [
-    'img/4_enemie_boss_chicken/3_attack/G13.png',
-    'img/4_enemie_boss_chicken/3_attack/G14.png',
-    'img/4_enemie_boss_chicken/3_attack/G15.png',
-    'img/4_enemie_boss_chicken/3_attack/G16.png',
-    'img/4_enemie_boss_chicken/3_attack/G17.png',
-    'img/4_enemie_boss_chicken/3_attack/G18.png',
-    'img/4_enemie_boss_chicken/3_attack/G19.png',
-    'img/4_enemie_boss_chicken/3_attack/G20.png',
-  ];
-
-  images_hurt = [
-    'img/4_enemie_boss_chicken/4_hurt/G21.png',
-    'img/4_enemie_boss_chicken/4_hurt/G22.png',
-    'img/4_enemie_boss_chicken/4_hurt/G23.png'
-  ];
-
-  images_dead = [
-    'img/4_enemie_boss_chicken/5_dead/G24.png',
-    'img/4_enemie_boss_chicken/5_dead/G25.png',
-    'img/4_enemie_boss_chicken/5_dead/G26.png'
-  ];
-
   constructor() {
-    super().loadImage(this.images_walking[0]);
-
-    this.loadImages(this.images_walking);
-    this.loadImages(this.images_alert);
-    this.loadImages(this.images_attack);
-    this.loadImages(this.images_hurt);
-    this.loadImages(this.images_dead);
-
+    super();
     this.x = 2500;
-
+    this.loadImage(endboss_assets.walking[0]);
+    this.preloadImages();
     this.animate();
   }
 
+  preloadImages() {
+    this.loadImages(endboss_assets.walking);
+    this.loadImages(endboss_assets.alert);
+    this.loadImages(endboss_assets.attack);
+    this.loadImages(endboss_assets.hurt);
+    this.loadImages(endboss_assets.dead);
+  }
+
   animate() {
-    setInterval(() => {
-      if (this.isDead) return this.playAnimation(this.images_dead);
-      if (this.isHurtBoss) return this.playAnimation(this.images_hurt);
-      if (!this.isActive) return this.playAnimation(this.images_walking);
+    setInterval(() => this.animateSprite(), 160);
+    setInterval(() => this.updateMovementAndAttack(), 1000 / 60);
+  }
 
-      if (this.currentState === "alert") return this.playAnimation(this.images_alert);
-      if (this.currentState === "attack") return this.playAnimation(this.images_attack);
+  animateSprite() {
+    if (this.isDead) return this.playAnimation(endboss_assets.dead);
+    if (this.isHurtBoss) return this.playAnimation(endboss_assets.hurt);
+    if (!this.isActive) return this.playAnimation(endboss_assets.walking);
 
-      return this.playAnimation(this.images_walking);
-    }, 160);
+    if (this.currentState === "alert") return this.playAnimation(endboss_assets.alert);
+    if (this.currentState === "attack") return this.playAnimation(endboss_assets.attack);
 
-    setInterval(() => {
-      if (!this.isActive || this.isDead || !this.world) return;
-      const character = this.world.character;
-      const distance = Math.abs(character.x - this.x);
-      if (this.currentState === "alert") return;
-      if (distance < 95) {
-        this.attack(character);
-        return;
-      }
-      this.currentState = "walk";
-      if (character.x < this.x) {
-        this.otherDirection = false;
-        this.x -= this.speed;
-      } else {
-        this.otherDirection = true;
-        this.x += this.speed;
-      }
-    }, 1000 / 60);
+    return this.playAnimation(endboss_assets.walking);
+  }
+
+  updateMovementAndAttack() {
+    if (!this.isActive || this.isDead || !this.world) return;
+
+    const character = this.world.character;
+    const distance = Math.abs(character.x - this.x);
+
+    if (this.currentState === "alert") return;
+
+    if (distance < 95) {
+      this.attack(character);
+      return;
+    }
+
+    this.currentState = "walk";
+
+    if (character.x < this.x) {
+      this.otherDirection = false;
+      this.x -= this.speed;
+    } else {
+      this.otherDirection = true;
+      this.x += this.speed;
+    }
   }
 
   setActive() {
     if (this.isActive) return;
+
     this.isActive = true;
     this.currentState = "alert";
 
@@ -133,6 +100,7 @@ class Endboss extends MovableObject {
     if (this.isDead) return;
 
     this.energy -= dmg;
+
     if (this.energy <= 0) {
       this.energy = 0;
       this.die();
