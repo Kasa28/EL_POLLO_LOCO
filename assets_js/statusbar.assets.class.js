866f478cbbc92d297b761ff48b564546fb914468
Kasa28 <agliullinaksenija@gmail.com>
2026-02-09 17:58:37 +0100

    folder for assets

---
+++ assets_js/statusbar.assets.class.js
@@ -0,0 +1,37 @@
+const statusbar_assets = {
+  health: [
+    "img/7_statusbars/1_statusbar/2_statusbar_health/blue/0.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/blue/20.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/blue/40.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/blue/60.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/blue/80.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png",
+  ],
+
+  coins: [
+    "img/7_statusbars/1_statusbar/1_statusbar_coin/blue/0.png",
+    "img/7_statusbars/1_statusbar/1_statusbar_coin/blue/20.png",
+    "img/7_statusbars/1_statusbar/1_statusbar_coin/blue/40.png",
+    "img/7_statusbars/1_statusbar/1_statusbar_coin/blue/60.png",
+    "img/7_statusbars/1_statusbar/1_statusbar_coin/blue/80.png",
+    "img/7_statusbars/1_statusbar/1_statusbar_coin/blue/100.png",
+  ],
+
+  bottles: [
+    "img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/0.png",
+    "img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/20.png",
+    "img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/40.png",
+    "img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/60.png",
+    "img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/80.png",
+    "img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/100.png",
+  ],
+
+  boss: [
+    "img/7_statusbars/2_statusbar_endboss/blue/blue0.png",
+    "img/7_statusbars/2_statusbar_endboss/blue/blue20.png",
+    "img/7_statusbars/2_statusbar_endboss/blue/blue40.png",
+    "img/7_statusbars/2_statusbar_endboss/blue/blue60.png",
+    "img/7_statusbars/2_statusbar_endboss/blue/blue80.png",
+    "img/7_statusbars/2_statusbar_endboss/blue/blue100.png",
+  ],
+};
--- models/status_bar.class.js
+++ models/status_bar.class.js
@@ -4,67 +4,24 @@ class StatusBar extends DrawableObject {
 
   constructor(images, x, y, startPercentage = 0) {
     super();
-    this.images = images;          
+    this.images = images;
     this.loadImages(this.images);
     this.x = x;
     this.y = y;
     this.width = 200;
     this.height = 60;
-    this.setPercentage(startPercentage);
 
+    this.setPercentage(startPercentage);
   }
 
   setPercentage(percentage) {
     this.percentage = percentage;
-    let path = this.images[this.resolveImageIndex()];
+    const path = this.images[this.resolveImageIndex()];
     this.img = this.imageCache[path];
   }
 
   resolveImageIndex() {
-    if(this.percentage <= 0) return 0;
-    return Math.min(5, Math.ceil(this.percentage/ 20));
+    if (this.percentage <= 0) return 0;
+    return Math.min(5, Math.ceil(this.percentage / 20));
   }
 }
-
-
-const images_health = [
-        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/0.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/20.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/40.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/60.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/80.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png'
-    ];
-
-const images_coins = [
-        'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/0.png',
-        'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/20.png',
-        'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/40.png',
-        'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/60.png',
-        'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/80.png',
-        'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/100.png'
-
-    ];
-
-const images_bootles = [
-        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/0.png',
-        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/20.png',
-        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/40.png',
-        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/60.png',
-        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/80.png',
-        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/100.png'
-
-    ];
-
-    const images_boss = [
-        'img/7_statusbars/2_statusbar_endboss/blue/blue0.png',
-        'img/7_statusbars/2_statusbar_endboss/blue/blue20.png',
-        'img/7_statusbars/2_statusbar_endboss/blue/blue40.png',
-        'img/7_statusbars/2_statusbar_endboss/blue/blue60.png',
-        'img/7_statusbars/2_statusbar_endboss/blue/blue80.png',
-        'img/7_statusbars/2_statusbar_endboss/blue/blue100.png',
-
-
-    ];
-
-
