b720bfa5a08141cfb046deddf49c624ddd28d58b
Kasa28 <agliullinaksenija@gmail.com>
2026-02-10 14:22:08 +0100

    remove images

--- models/throwable_object.class.js
+++ models/throwable_object.class.js
@@ -1,29 +1,12 @@
 class ThrowableObject extends MovableObject {
   width = 50;
   height = 100;
-
-  images_rotation = [
-    "img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
-    "img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
-    "img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png",
-    "img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png"
-  ];
-
-  images_splash = [
-    "img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png",
-    "img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png",
-    "img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png",
-    "img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png",
-    "img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png",
-    "img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png"
-  ];
-
   groundY = 450;
 
   constructor(x, y, throwRight = true) {
-    super().loadImage(this.images_rotation[0]);
-    this.loadImages(this.images_rotation);
-    this.loadImages(this.images_splash);
+    super().loadImage(throwable_assets.rotation[0]);
+    this.loadImages(throwable_assets.rotation);
+    this.loadImages(throwable_assets.splash);
     this.setStartPosition(x, y);
     this.setThrowDirection(throwRight);
     this.resetThrowPhysics();
@@ -91,13 +74,12 @@ class ThrowableObject extends MovableObject {
 
   updateRotationFrame() {
     if (this.isSplashed) return;
-    this.playAnimation(this.images_rotation);
+    this.playAnimation(throwable_assets.rotation);
   }
 
   splash() {
     if (this.isSplashed) return;
     this.isSplashed = true;
-
     this.stopFlightLoop();
     this.stopRotationLoop();
     this.startSplashAnimation();
@@ -114,7 +96,7 @@ class ThrowableObject extends MovableObject {
 
   startSplashAnimation() {
     this.splashInterval = setInterval(() => {
-      this.playAnimation(this.images_splash);
+      this.playAnimation(throwable_assets.splash);
     }, 60);
   }
 
