5b7a1cffb6925ec5c165e28e69530fe996cf5cb1
Kasa28 <agliullinaksenija@gmail.com>
2026-02-11 02:06:49 +0100

    chore empty space

--- models/bottle_under.class.js
+++ models/bottle_under.class.js
@@ -6,10 +6,8 @@ class BottlePickup extends MovableObject {
   constructor(x) {
     super();
     this.loadImages(bottle_assets.images_bottles);
-
     const randomImg = bottle_assets.images_bottles[Math.floor(Math.random() * bottle_assets.images_bottles.length)];
     this.loadImage(randomImg);
-
     this.x = x ?? (200 + Math.random() * 1800);
   }
 }
