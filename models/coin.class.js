e216d497edacc0f0549d297953cb2d37e3442805
Kasa28 <agliullinaksenija@gmail.com>
2026-02-11 17:59:00 +0100

    js doc

--- models/coin.class.js
+++ models/coin.class.js
@@ -1,11 +1,25 @@
+/**
+ * Collectible coin object.
+ * @extends MovableObject
+ */
 class Coin extends MovableObject {
+  /** @type {number} */
   width = 80;
+
+  /** @type {number} */
   height = 80;
 
+  /**
+   * Creates a coin at a given position or at a random position if not provided.
+   * @param {number} [x] - Initial x-position. If omitted, a random x is used.
+   * @param {number} [y] - Initial y-position. If omitted, a random y is used.
+   */
   constructor(x, y) {
     super();
     this.loadImage(coin_assets.icon);
+    /** @type {number} */
     this.x = x ?? (200 + Math.random() * 1800);
+    /** @type {number} */
     this.y = y ?? (80 + Math.random() * 120);
   }
 }
