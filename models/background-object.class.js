5c129b2ae45daa4906daecabe7a7edc441140a83
Kasa28 <agliullinaksenija@gmail.com>
2026-02-11 17:45:39 +0100

    refactor: add JSDoc for BackgroundObject constructor parameters

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,10 +1,15 @@
-class BackgroundObject extends MovableObject{
- width = 720;
- height = 500;
+class BackgroundObject extends MovableObject {
+  width = 720;
+  height = 500;
 
-    constructor(imagePath, x, y){
-        super().loadImage(imagePath);
-        this.x = x;
-        this.y = 480 - this.height;
-    }
+  /**
+   * @param {string} imagePath
+   * @param {number} x
+   * @param {number} y
+   */
+  constructor(imagePath, x, y) {
+    super().loadImage(imagePath);
+    this.x = x;
+    this.y = 480 - this.height;
+  }
 }
