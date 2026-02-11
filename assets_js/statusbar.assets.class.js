8c0cb4988c74a604c6e5d3eff5958b64d3891d03
Kasa28 <agliullinaksenija@gmail.com>
2026-02-11 18:45:02 +0100

    fix img

--- assets_js/statusbar.assets.class.js
+++ assets_js/statusbar.assets.class.js
@@ -28,7 +28,7 @@ const statusbar_assets = {
   bottles: [
     "img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/0.png",
     "img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/20.png",
-    "img/7_statusbars/1_statusbars/1_statusbar/3_statusbar_bottle/blue/40.png",
+    "img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/40.png",
     "img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/60.png",
     "img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/80.png",
     "img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/100.png",
