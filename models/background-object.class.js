27f051a36c408b4b318f4dd18b9a938a7b6688e0
Kasa28 <agliullinaksenija@gmail.com>
2026-02-11 02:06:19 +0100

    chore clean space

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -4,7 +4,6 @@ class BackgroundObject extends MovableObject{
 
     constructor(imagePath, x, y){
         super().loadImage(imagePath);
-
         this.x = x;
         this.y = 480 - this.height;
     }
