aa3952c81656e3d77d9fb7416d063d647920d71d
Kasa28 <agliullinaksenija@gmail.com>
2026-02-11 17:48:17 +0100

    hore: add BottlePickup class for bottle collectibles

--- models/bottle_under.class.js
+++ models/bottle_under.class.js
@@ -3,10 +3,16 @@ class BottlePickup extends MovableObject {
   height = 80;
   y = 350;
 
+  /**
+   * @param {number} [x]
+   */
   constructor(x) {
     super();
     this.loadImages(bottle_assets.images_bottles);
-    const randomImg = bottle_assets.images_bottles[Math.floor(Math.random() * bottle_assets.images_bottles.length)];
+    const randomImg =
+      bottle_assets.images_bottles[
+        Math.floor(Math.random() * bottle_assets.images_bottles.length)
+      ];
     this.loadImage(randomImg);
     this.x = x ?? (200 + Math.random() * 1800);
   }
